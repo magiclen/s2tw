6a1621c67d65546a9e9e34287d8e12e73f1d0d6e
Magic Len <len@magiclen.org>
2023-11-13 22:11:38 +0800

    remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-//! # s2tw
-//! A simple tool for converting Simple Chinese to Traditional Chinese(TW).
+/*!
+# s2tw
 
-use std::{fs, path::Path};
-
-pub fn try_delete<P: AsRef<Path>>(path: P) {
-    if fs::remove_file(path.as_ref()).is_err() {}
-}
+A simple tool for converting Simple Chinese to Traditional Chinese(TW).
+*/
